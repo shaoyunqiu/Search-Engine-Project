f9a5fbf541b738463e02ccf236d422ad75e14be0
s0846 <shaoyq14@mails.tsinghua.edu.cn>
2017-05-30 10:34:28 +0800

    add content in vertical res

--- src/VerticalIndexs.java
+++ src/VerticalIndexs.java
@@ -11,9 +11,11 @@ public class VerticalIndexs {
 	public Map<Integer, Integer> idMap ;
 	public ArrayList<String> self_url ;
 	public ArrayList<String> title ;
+	public ArrayList<String> content ;
 	public int[][] choices ;
 	public String[][] childurl ;
 	public String[][] childtitle ;
+	public String[][] childcontent ;
 	public int sum ;
 	
 	public VerticalIndexs() {
@@ -21,6 +23,7 @@ public class VerticalIndexs {
 		idMap = new HashMap<Integer, Integer>() ;
 		self_url = new ArrayList<String>() ;
 		title = new ArrayList<String>() ;
+		content = new ArrayList<String>() ;
 		choices = new int[13][4] ;
 		sum = 13 ;
 		File file= new File("../indexs/record.txt") ;
@@ -30,13 +33,14 @@ public class VerticalIndexs {
 			String lineString = null ;
 			while((lineString = bufferedReader.readLine()) != null){
 				String[] after = lineString.split(" ") ;
-				if(after.length < 3) continue ;
+				if(after.length < 4) continue ;
 				else{
 					++ cnt ;
 					Integer id = Integer.valueOf(after[0]) ;
 					idMap.put(id, cnt) ;
 					title.add(after[1]) ;
 					self_url.add(after[2]) ;
+					content.add(after[3]) ;
 				}
 			}
 			bufferedReader.close();
@@ -53,17 +57,19 @@ public class VerticalIndexs {
 		file = new File("../indexs/child1.txt") ;
 		childurl = new String[2][4] ;
 		childtitle = new String[2][4] ;
+		childcontent = new String[2][4] ;
 		cnt = -1 ;
 		try {
 			BufferedReader bufferedReader = new BufferedReader(new FileReader(file)) ;
 			String lineString = null ;
 			while((lineString = bufferedReader.readLine()) != null){
 				String[] after = lineString.split(" ") ;
-				if(after.length < 2) continue ;
+				if(after.length < 3) continue ;
 				else{
 					++ cnt ;
 					childtitle[0][cnt] = after[0] ;
 					childurl[0][cnt] = after[1] ;
+					childcontent[0][cnt] = after[2] ;
 				}
 			}
 			bufferedReader.close();
@@ -83,6 +89,7 @@ public class VerticalIndexs {
 					++ cnt ;
 					childtitle[1][cnt] = after[0] ;
 					childurl[1][cnt] = after[1] ;
+					childcontent[1][cnt] = after[2] ;
 				}
 			}
 			bufferedReader.close();
@@ -100,19 +107,21 @@ public class VerticalIndexs {
 	}
 	
 	public String[][] getVertical(Integer docid){
-		String[][] res = new String[2][4] ;
+		String[][] res = new String[3][4] ;
 		if(hasVerticla(docid)){
 			int index = idMap.get(docid) ;
 			if(index < 2){
 				for(int i = 0 ; i < 4 ; i ++){
 					res[0][i] = childtitle[index][i] ;
 					res[1][i] = childurl[index][i] ;
+					res[2][i] = childcontent[index][i] ;
 				}
 			}
 			else {
 				for(int i = 0 ; i < 4 ; i ++){
 					res[0][i] = title.get(choices[index][i]) ;
 					res[1][i] = self_url.get(choices[index][i]) ;
+					res[2][i] = content.get(choices[index][i]) ;
 				}
 			}
 		}else {
@@ -125,12 +134,12 @@ public class VerticalIndexs {
 		return res ;
 	}
 	
-	public static void main(String[] argv) {
+	/*public static void main(String[] argv) {
 		VerticalIndexs vi = new VerticalIndexs() ;
 		if(vi.hasVerticla(26168)){
 			String[][] v = vi.getVertical(26168) ;
 			for(int i = 0 ; i < 4 ; i ++){
-				System.out.println(v[0][i] + "\t" + v[1][i]);
+				System.out.println(v[0][i] + "\t" + v[1][i] + "\t" + v[2][i]);
 			}
 		}else {
 			System.out.println("26168 has no vertical res");
@@ -139,7 +148,7 @@ public class VerticalIndexs {
 		if(vi.hasVerticla(28443)){
 			String[][] v = vi.getVertical(28443) ;
 			for(int i = 0 ; i < 4 ; i ++){
-				System.out.println(v[0][i] + "\t" + v[1][i]);
+				System.out.println(v[0][i] + "\t" + v[1][i] + "\t" + v[2][i]);
 			}
 		}else {
 			System.out.println("28443 has no vertical res");
@@ -148,11 +157,11 @@ public class VerticalIndexs {
 		if(vi.hasVerticla(100)){
 			String[][] v = vi.getVertical(26168) ;
 			for(int i = 0 ; i < 4 ; i ++){
-				System.out.println(v[0][i] + "\t" + v[1][i]);
+				System.out.println(v[0][i] + "\t" + v[1][i] + "\t" + v[2][i]);
 			}
 		}else {
 			System.out.println("100 has no vertical res");
 		}
-	}
+	}*/
 }
 
